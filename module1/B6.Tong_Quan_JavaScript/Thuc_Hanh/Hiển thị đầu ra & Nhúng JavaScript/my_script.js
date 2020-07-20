faf12fe35888e9bbbeff69d34e372cdfa6795aac
thanhdat1320 <nhtd.1320@gmail.com>
2020-07-20 19:27:59 +0700

    finish

---
+++ module1/B6.Tong_Quan_JavaScript/Thuc_Hanh/Hiển thị đầu ra & Nhúng JavaScript/my_script.js
@@ -0,0 +1,17 @@
+// BT.Hiển thị kết quả sử dụng hàm alert():
+function showMessage() {
+    alert('Xin chào!');
+}
+//BT2.Hiển thị kết quả sử dụng thẻ HTML:
+function showMessage2(){
+    document.getElementById('messageDiv').innerHTML = 'Xin Chào';
+}
+//BT3.Hiển thị kết quả sử dụng hàm document.write():
+function showMessage3() {
+    document.write('Xin Chào');
+}
+//BT4.Hiển thị kết quả sử dụng hàm console.log():
+function showMessage4(){
+    console.log('Xin chào!');
+}
+
