788266532a04201d5947ffcbe31df13d6fdd0fb2
thanhdat1320 <nhtd.1320@gmail.com>
2020-07-23 17:00:46 +0700

    finish

--- module1/CaseStudy/Yêu Cầu 1/Task_1.js
+++ module1/CaseStudy/Yêu Cầu 1/Task_1.js
@@ -73,7 +73,7 @@ function showAllInformation(){
             "</tr>" +
             "<tr>" +
                 "<td>Total:</td>" +
-                "<td>" + total + " " + "USD"+"</td>" +
+                "<td  class='totalAll'>" + total + " " + "USD"+"</td>" +
             "</tr>" +
         "</table>"
 }
