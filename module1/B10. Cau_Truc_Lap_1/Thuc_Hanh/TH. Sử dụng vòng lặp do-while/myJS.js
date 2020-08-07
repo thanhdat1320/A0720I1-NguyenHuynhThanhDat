7e0dfcad97bf1173b500ca2ee57ebb5be0cfbba0
thanhdat1320 <nhtd.1320@gmail.com>
2020-08-07 16:35:47 +0700

    fisish

---
+++ module1/B10. Cau_Truc_Lap_1/Thuc_Hanh/TH. Sử dụng vòng lặp do-while/myJS.js
@@ -0,0 +1,4 @@
+do {
+    value = prompt("Enter a number 1 -> 10:");
+} while (value < 1 || value >10);
+alert(value);
--- module1/B10. Cau_Truc_Lap_1/Thuc_Hanh/TH. Sử dụng vòng lặp while/myJS.js
+++ module1/B10. Cau_Truc_Lap_1/Thuc_Hanh/TH. Sử dụng vòng lặp while/myJS.js
@@ -1,8 +1,8 @@
 let total = 0;
 let num = parseFloat(prompt("Enter a number :"));
 while (num != -1) {
+    num = parseFloat(prompt("Enter a number:"));
+    alert(num);
     total += num;
-    document.getElementById("showResult").innerHTML = num;
-    num = parseFloat("Enter a number:");
 }
 document.getElementById("showTotal").innerHTML = total;
