e8428186706e7005ea1493903c65841952767f25
= <=>
2020-09-29 15:49:05 +0700

    finish

---
+++ Module02/src/B3_Mang_PhuongThuc/Thuc_Hanh/ValueMaxInArray.java
@@ -0,0 +1,14 @@
+package B3_Mang_PhuongThuc.Thuc_Hanh;
+
+import java.util.Scanner;
+
+public class ValueMaxInArray {
+
+/*    public static void maxValue(int array) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = Scanner(System.in);
+    }*/
+}
